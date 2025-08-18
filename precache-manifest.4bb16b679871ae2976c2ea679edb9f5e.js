a437b04585fab8ebf3cfb64ac0eb99a93568f8a5
adadachao <adadachao@gmail.com>
2025-08-18 18:48:23 +0800

    Add files via upload
    usd1swap explorer up

---
+++ precache-manifest.4bb16b679871ae2976c2ea679edb9f5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d104c5caea7392bb33b1aa58dcabf1f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42250cffd640c066b5c2",
+    "url": "./static/js/2.b0f2c42f.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "./static/js/2.b0f2c42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627ffaaaa27eab56d696",
+    "url": "./static/js/main.1a1e915c.chunk.js"
+  },
+  {
+    "revision": "51cca0025286cb519c68",
+    "url": "./static/js/runtime-main.390e6a21.js"
+  },
+  {
+    "revision": "94e173deb0980ec7ee72b38d7378e8ff",
+    "url": "./static/media/logo.94e173de.svg"
+  },
+  {
+    "revision": "29e5e4e67ebbf4f27bffa2fc840d68dc",
+    "url": "./static/media/logo_white.29e5e4e6.svg"
+  },
+  {
+    "revision": "17391abb107005d92aef3cdd637881a9",
+    "url": "./static/media/mars.17391abb.png"
+  }
+]);
